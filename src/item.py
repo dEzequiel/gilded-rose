8ddc7f9bcc2f525be7c2f6a220d88e214e702ffe
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-01-25 18:04:43 +0100

    feat(Item): external file to create independent items

---
+++ src/item.py
@@ -0,0 +1,7 @@
+class Item:
+    # This is just a normal item, no logic, just attributes.
+    def __init__(self, name, sell_in, quality):
+
+        self.name = name
+        self.sell_in = sell_in
+        self.quality = quality
