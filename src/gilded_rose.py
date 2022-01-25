57c7e07d9f2b38715720ace801cf78e113ab73a6
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-01-25 21:23:28 +0100

    feat(Normal_Item): add class and methods to normal type items

--- src/gilded_rose.py
+++ src/gilded_rose.py
@@ -1,4 +1,5 @@
 from ast import Expression, expr
+from cmath import exp
 from random import expovariate
 from winreg import ExpandEnvironmentStrings
 from src.item import Item
@@ -35,9 +36,6 @@ class Item:
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
-class Interface(Item):
-    def update_quality():
-        pass
 
 class Stock_Item(Item):
 
@@ -58,6 +56,21 @@ class Stock_Item(Item):
 
     def update_quality(self):
         self.reduce_quality(2) if self.expired_item() else self.reduce_quality(1)
+
+class Normal_Item(Stock_Item):
+
+    def expired_counter(self):
+        self.sell_in -= 1
+
+    def set_quality(self, value):
+        self.quality = value
+
+    def update_quality(self):
+        if self.expired_item():
+            self.set_quality(MIN_ITEM_QUALITY) 
+        else:
+            self.expired_counter()
+            self.reduce_quality(1)
 
 class Aged_Brie(Stock_Item):
 
@@ -80,3 +93,8 @@ class Backstage_Pass(Stock_Item):
             self.improve_quality(2)
         else:
             self.improve_quality(1)
+
+# class Conjured_Item(Stock_Item):
+    
+#     def update_quality(self):
+#         self.reduce_quality(4)
