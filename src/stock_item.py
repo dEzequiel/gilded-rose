0c2dee3d0ab840b2f6e46487142d9cffcc9a743b
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-01-31 23:33:39 +0100

    refactor(stock_item) delete unnecesary methods, just viable ones

--- src/stock_item.py
+++ src/stock_item.py
@@ -2,30 +2,27 @@ from values import Value
 from gilded_rose import Item
 
 class Stock_Item(Item):
-
-    def set_quality(self, amount):
-        
-        if amount > Value.MAX_ITEM_QUALITY.value:
-            self.quality = Value.MAX_ITEM_QUALITY.value
-        else:
-            self.quality = amount
+    
+    def get_sell_in(self):
+        return self.sell_in
+    
+    def get_quality(self):
+        return self.quality
 
     def decrease_sell_in(self):
-        if self.sell_in <= Value.ZERO.value:
-            self.set_quality(Value.ZERO.value)
-
         self.sell_in -= Value.ONE.value
 
     def improve_quality(self, amount):
         self.quality += amount
 
-        if self.quality > Value.MAX_ITEM_QUALITY.value:
-            self.quality = Value.MAX_ITEM_QUALITY.value
-
     def reduce_quality(self, amount):
         self.quality -= amount
-        if self.quality < Value.ZERO.value:
-            self.quality = Value.MIN_ITEM_QUALITY.value
+    
+    def quality_checker(self):
+        if self.get_quality > Value.MAX_ITEM_QUALITY.value:
+            self.quality = Value.MAX_ITEM_QUALITY
+    
+
 
     # def update_quality(self):
     #     self.reduce_quality(Value.TWO) if self.expired_sell_in() else self.reduce_quality(Value.ONE)
