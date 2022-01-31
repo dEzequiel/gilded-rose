13cd8bcfd255cf8981e3b137f734ddac683ca786
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-01-31 23:46:08 +0100

    refactor(stock_item): add quality_checker method

--- src/stock_item.py
+++ src/stock_item.py
@@ -8,19 +8,26 @@ class Stock_Item(Item):
     
     def get_quality(self):
         return self.quality
+    
+    def set_quality(self, amount):
+        self.quality = amount
+        self.quality_checker()
 
     def decrease_sell_in(self):
         self.sell_in -= Value.ONE.value
 
     def improve_quality(self, amount):
         self.quality += amount
+        self.quality_checker()
+
 
     def reduce_quality(self, amount):
         self.quality -= amount
+        self.quality_checker()
     
     def quality_checker(self):
-        if self.get_quality > Value.MAX_ITEM_QUALITY.value:
-            self.quality = Value.MAX_ITEM_QUALITY
+        if self.get_quality() > Value.MAX_ITEM_QUALITY.value:
+            self.quality = Value.MAX_ITEM_QUALITY.value
     
 
 
