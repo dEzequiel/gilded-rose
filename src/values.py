6a5be7800f2789d1bb4e9698c430c45836cd409f
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-01-31 19:52:59 +0100

    feat(Values): create enumerate class

---
+++ src/values.py
@@ -0,0 +1,15 @@
+from enum import IntEnum
+
+class Values(IntEnum):
+
+    TEN_DAYS = 10
+    FIVE_DAYS = 5
+    MAX_ITEM_QUALITY = 50
+    MIN_ITEM_QUALITY = 0
+    LEGENDARY_ITEM_QUALITY = 80    
+
+    ZERO = 0
+    ONE = 1
+    TWO = 2
+    THREE = 3
+    FOUR = 4
