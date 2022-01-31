7b11f5e3f01ab4b4dde1e656f05ddc3d7c3cb83a
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-01-31 21:36:07 +0100

    refactor(stock_item): use of enum value and less methods

--- src/stock_item.py
+++ src/stock_item.py
@@ -1,33 +1,28 @@
 from values import Value
+from gilded_rose import Item
 
 class Stock_Item(Item):
 
-    def set_quality(self, value):
+    def set_quality(self, amount):
         
-        if value > Value.MAX_ITEM_QUALITY:
-            self.quality = Value.MAX_ITEM_QUALITY
+        if amount > Value.MAX_ITEM_QUALITY.value:
+            self.quality = Value.MAX_ITEM_QUALITY.value
         else:
-            self.quality = self.value
-        
-    def expired_sell_in(self):
+            self.quality = amount
 
-        if self.sell_in == Value.ZERO:
-            self.sell_in = Value.ZERO
-        else:
-            self.sell_in -= Value.ONE
-            self.reduce_quality(Value.ONE)
-        
     def decrease_sell_in(self):
-        self.sell_in -= Value.ONE
+        if self.sell_in <= Value.ZERO.value:
+            self.set_quality(Value.ZERO.value)
 
-    def improve_quality(self, value):
-        self.quality += value
-    
-    def reduce_quality(self, value):
-        self.quality -= value
-        
-        if self.quality < Value.ZERO:
-            self.quality = Value.MIN_ITEM_QUALITY
+        self.sell_in -= Value.ONE.value
+
+    def improve_quality(self, amount):
+        self.quality += amount
+
+    def reduce_quality(self, amount):
+        self.quality -= amount
+        if self.quality < Value.ZERO.value:
+            self.quality = Value.MIN_ITEM_QUALITY.value
 
     # def update_quality(self):
     #     self.reduce_quality(Value.TWO) if self.expired_sell_in() else self.reduce_quality(Value.ONE)
