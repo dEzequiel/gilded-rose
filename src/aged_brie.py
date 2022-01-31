d3f5af3a4482914e77c6fbfd926fb6071773103d
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-01-31 21:49:26 +0100

    feat(aged_brie): overwrite decrease_sell_in for aged brie type items

--- src/aged_brie.py
+++ src/aged_brie.py
@@ -1,4 +1,21 @@
-from gilded_rose import Gilded_Rose, Aged_Brie
+from gilded_rose import Gilded_Rose
+from stock_item import Stock_Item
+from values import Value
+
+class Aged_Brie(Stock_Item):
+    
+    def decrease_sell_in(self):
+
+        if self.sell_in <= Value.ZERO.value:
+            self.improve_quality(Value.TWO.value)
+        else:
+            self.improve_quality(Value.ONE.value)
+
+        self.sell_in -= Value.ONE.value    
+
+    def update_quality(self):
+        self.decrease_sell_in()
+
 
 if __name__ == '__main__': 
 
