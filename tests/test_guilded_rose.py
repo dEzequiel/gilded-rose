2ef3aa36bd0e580e494116a7bbf9190765b7aaa0
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-02-01 02:56:36 +0100

    test(test_gilded_rose): test item constructor and item representation

--- tests/test_guilded_rose.py
+++ tests/test_guilded_rose.py
@@ -1,14 +1,14 @@
-import unittest
+import pytest
+from src.gilded_rose import Item
 
+@pytest.mark.test_item_constructor
+def test_item_constructor():
+    item = Item("Help me!", 9, 9)
+    assert "Help me!" == item.name
+    assert 9 == item.quality
+    assert 9 == item.sell_in
 
-from src.gilded_rose import Stock_Item, Gilded_Rose
-
-class GildedRoseTest(unittest.main):
-    def test_item_decreases_quality_by_one(self):
-        items = [Stock_Item("Elixir of the Mongoose,", 5, 5)]
-        gilded_rose = Gilded_Rose(items)
-        gilded_rose.update_quality()
-        self.assertEqual(4, items[0].quality)
-
-if __name__ == '__main__':
-    unittest.main()
+@pytest.mark.test_item_representation
+def test_item_representation():
+    item = Item("Help me!", 9, 9)
+    assert item.__repr__() == "Help me!, 9, 9" 
