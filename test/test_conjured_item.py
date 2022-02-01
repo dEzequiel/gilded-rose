470881a71bfc4fe41191d5f6ce610b627bbe6663
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-02-01 19:51:27 +0100

    test(conjured_item): fix test for new logic behaviour

--- test/test_conjured_item.py
+++ test/test_conjured_item.py
@@ -6,7 +6,7 @@ def test_conjured_item_IF():
     conjured_item = Conjured_Item("Conjured Mana Cake", 3, 6)
     conjured_item.update_quality()
 
-    assert 4 == conjured_item.quality
+    assert 5 == conjured_item.quality
     assert 2 == conjured_item.sell_in
 
 @pytest.mark.test_conjured_item_ELSE
@@ -14,5 +14,5 @@ def test_conjured_item_ELSE():
     conjured_item = Conjured_Item("Conjured Mana Cake", 0, 6)
     conjured_item.update_quality()
 
-    assert 2 == conjured_item.quality
+    assert 5 == conjured_item.quality
     assert -1 == conjured_item.sell_in
